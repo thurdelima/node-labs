d0183e16488971245ad5573f802beafeb43875b9
Arthur Lima <thurdelima@gmail.com>
2021-05-13 12:17:19 -0300

    second commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,18 @@
 require('dotenv/config');
 
+let host_prod = 'isilo.db.elephantsql.com';
+let user_prod = 'ctbdxbhl';
+let password_prod = 'h7FDhQhHI3C7WHzmDJ0w7YMVNyp9lAW6';
+let database_prod = 'ctbdxbhl';
+
+
 
 module.exports = {
     dialect: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: host_prod,
+    username: user_prod,
+    password: password_prod,
+    database: database_prod,
     operadorAliases: false,
     define: {
       timestamps: true,
@@ -14,3 +20,17 @@ module.exports = {
       underscoredAll: true
     }
   };
+
+
+//CONFIG TO TEST LOCAL
+//   dialect: 'postgres',
+//     host: process.env.DB_HOST,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     operadorAliases: false,
+//     define: {
+//       timestamps: true,
+//       underscored: true,
+//       underscoredAll: true
+//     }
